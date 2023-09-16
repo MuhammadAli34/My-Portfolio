7b728c556265bb4e30bd1599a45b265b1029b442
Muhammad Ali <107616847+MuhammadAli34@users.noreply.github.com>
2023-09-16 16:23:57 +0300

    Update Project.tsx

--- components/Project.tsx
+++ components/Project.tsx
@@ -4,7 +4,7 @@ import { useRef } from "react";
 import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
-
+import { AiFillGithub } from "react-icons/ai";
 type ProjectProps = (typeof projectsData)[number];
 
 export default function Project({
@@ -33,8 +33,11 @@ export default function Project({
       <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70 flex">
             {description}
+              <a href={link} target="_blank">
+              <AiFillGithub className="w-[40px] h-[40px] hover:text-blue-400 duration-300 ease-in-out" />
+            </a>
           </p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
