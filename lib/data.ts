8cc8bd5fac2f2192ffa1ff994b1db6b4b1f0bf14
Muhammad Ali <107616847+MuhammadAli34@users.noreply.github.com>
2024-04-17 19:42:22 +0200

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -3,6 +3,8 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/First Project.png";
+import mamoh from "@/public/Mamoh.png";
+import esim from "@/public/esim.jpg";
 import rmtdevImg from "@/public/untitled.png";
 import third from "@/public/third.png";
 
@@ -32,6 +34,22 @@ export const links = [
 
 export const projectsData = [
   {
+    title: " Mamoh App Website",
+    description:
+      "I worked as the Web Dev of this website for Mamo application,It is a great application that is a smart search engine for buying and selling, and it was a wonderful experience to be part of this project. ",
+    tags: ["React", "Tailwind", "FramerMotion", "And Others"],
+    imageUrl: mamoh,
+    link: "https://www.mamoh.app",
+  },
+  {
+    title: "Esim",
+    description:
+      "Worked on a complete store for a telecommunications company in the Emirates and Saudi Arabia to create a platform for selling Esim purchases and a full App of the platform using a Flutter and many techniques, and it was a wonderful experience. ",
+    tags: ["React", "FireBase", "Tailwind", "straipe", "And Others"],
+    imageUrl: esim,
+    link: "",
+  },
+  {
     title: "Fashion Shop",
     description:
       "Full E Commerce shop Websiteو You can buy products and add it to Cart and sign in and logout with Firebase authentication. ",
@@ -46,15 +64,14 @@ export const projectsData = [
       "Full realtime chatapp with mern stack mongoDb,express,reactjs,node js and socket.io and a lot of other technology.",
     tags: ["MERN-STACK", "styled-components", "socket.io"],
     imageUrl: third,
-     link: "https://github.com/MuhammadAli34/Mern-Chat-App",
+    link: "https://github.com/MuhammadAli34/Mern-Chat-App",
   },
   {
     title: "E-commerce",
-    description:
-      "Full E Commerce shop Website.",
+    description: "Full E Commerce shop Website.",
     tags: ["React"],
     imageUrl: rmtdevImg,
-     link: "https://github.com/MuhammadAli34/E-commerce",
+    link: "https://github.com/MuhammadAli34/E-commerce",
   },
 ] as const;
 
